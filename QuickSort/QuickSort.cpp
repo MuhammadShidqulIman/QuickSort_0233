b5e2976e124082d5f8bf635f30ec98ed341146f6
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-03-28 13:56:02 +0700

    rumus

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -19,7 +19,17 @@ void input() {
             cout << "\nMaksimum panjang array adalah 20" << endl;
     }
 
+    cout << "\n-------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n-------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
+
 
 int main()
 {
