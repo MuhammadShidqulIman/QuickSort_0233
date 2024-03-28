2c343ac39f1ce61f191c491bcd8ac92c75117966
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-03-28 13:32:29 +0700

    Masukan Variabel

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 
-
+//array of integers to hold value 
+int arr[20];
+int cmp_count = 0; //number of comparasion
+int mov_count = 0; //number of movements
+int n;
 
 int main()
 {
