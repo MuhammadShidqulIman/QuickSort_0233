674632506dd76014fe398014e12fae121f6ce287
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-03-28 14:02:46 +0700

    step 2

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -46,9 +46,14 @@ void q_short(int low, int high)
     int temp;
     int pivot, i, j;
     if (low > high) { //step 1
-
+        return;
     }
+
+    pivot = arr[low]; //step2
+
 }
+
+
 
 
 int main()
