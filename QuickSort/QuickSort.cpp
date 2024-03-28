40b7877833be7de835b884b325a4be3838ad4c20
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2024-03-28 13:41:50 +0700

    menginput

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -7,6 +7,20 @@ int cmp_count = 0; //number of comparasion
 int mov_count = 0; //number of movements
 int n;
 
+void input() {
+    while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
